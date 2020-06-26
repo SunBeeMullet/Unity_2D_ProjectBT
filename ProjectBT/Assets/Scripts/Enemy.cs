b97fbd3a9d44abcd79c7fc7be8c6997ac73dee24
SunBeeMullet <choiseoungho0427@gmail.com>
2020-06-26 00:40:17 -0700

    Unity Project (v 0.02)
    Content:
    
    -Enemy created
    -Enemy animation
    -Game manager created

---
+++ ProjectBT/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public int health;
+    public int score;
+
+    public float speed;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+    Animator anim;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+
+        anim = GetComponent<Animator>();
+    }
+
+    void OnHit(int dmg)
+    {
+        health -= dmg;
+        anim.SetInteger("Hit", 1);
+        Invoke("ReturnSprite",0.3f);
+        
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturnSprite()
+    {
+        anim.SetInteger("Hit", 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "OuterBorder")
+        {
+            Destroy(gameObject);
+        }else if(collision.gameObject.tag == "Bullets")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+            Destroy(collision.gameObject);
+        }
+    }
+}
