10e9bd8ee5f0877a07025d8de5b6e0a43d8ac214
SunBeeMullet <choiseoungho0427@gmail.com>
2020-06-25 20:00:18 -0700

    Unity Project (v 0.01)
    Re - Uploading

---
+++ ProjectBT/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+}
