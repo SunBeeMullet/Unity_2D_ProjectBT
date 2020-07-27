f3d515616c5782c7e6b3bf7f2b23627d98b8924f
SunBeeMullet <choiseoungho0427@gmail.com>
2020-07-27 02:34:14 -0700

    Unity Project (v 0.07)
    Contents:
    
    - Explosion animation and script

--- ProjectBT/Assets/Scripts/Explosion.cs
+++ ProjectBT/Assets/Scripts/Explosion.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void StartExplosion(string target)
+    {
+        anim.SetTrigger("OnExplosion");
+
+        switch (target)
+        {
+            case "S":
+                transform.localScale = Vector3.one * 0.7f;
+                break;
+            case "M":
+            case "P":
+                transform.localScale = Vector3.one * 1f;
+                break;
+            case "L":
+                transform.localScale = Vector3.one * 2f;
+                break;
+            case "B":
+                transform.localScale = Vector3.one * 3f;
+                break;
+        }
+    }
 }
